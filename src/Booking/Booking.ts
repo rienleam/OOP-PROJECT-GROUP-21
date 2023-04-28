76ab99c76d6a49b6dd0f6f3669b10431c1731a90
SOCHEAT.LUN <SOCHEAT.LUN@PNC.LAN>
2023-04-28 17:50:36 +0700

    Create folder Project

---
+++ src/Booking/Booking.ts
@@ -0,0 +1,12 @@
+export enum Meal {
+    VEGETARIAN,
+    VEGAN,
+    DAIRY_FREE,
+    HALAL,
+    HOSHER
+}
+export enum Ticket_Condition {
+    ECONOMEY_CLASSIC,
+    ECONOMEY_FLEX,
+    BUSINESS_CLASS
+}
